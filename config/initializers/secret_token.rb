6881bcdb1b98280e09e64766978a5577136c5633
Tasha25 <greennatasha@aol.com>
2013-11-06 09:16:50 -0500

    Created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ship2ship::Application.config.secret_token = '722a823be4d3226975eb64fad0481d0a7d3a5d1f91798ea4a20cae81a1cdfed55c748ba6c3c93806586d03897602dc3086e8b02bc2010511c28346c302137a21'
